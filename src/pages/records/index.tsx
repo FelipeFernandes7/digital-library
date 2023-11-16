bd5ca5d98c56ca3e22a2904fa36f80246a6bfd56
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-11-16 11:03:46 -0300

    ...

--- src/pages/records/index.tsx
+++ src/pages/records/index.tsx
@@ -5,7 +5,12 @@ import { useProduct } from "../../hooks";
 export function Records() {
   const { product } = useProduct();
   return (
-    <Chakra.Box display={"flex"} width={"100%"} flexDirection={"column"}>
+    <Chakra.Box
+      display={"flex"}
+      width={"100%"}
+      flexDirection={"column"}
+      mb={{ base: "5rem", md: "0" }}
+    >
       <Table products={product} />
     </Chakra.Box>
   );
